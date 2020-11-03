dd7a0a35ac1f6f15a0eaf7d57b6a680e2e2dd4bc
sergiozed7 <sergiozed16@gmail.com>
2020-11-03 21:28:59 +0100

    Login backend starting point

---
+++ app/src/main/java/com/example/pastachat/Preloader.java
@@ -0,0 +1,33 @@
+package com.example.pastachat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class Preloader extends AppCompatActivity {
+
+    private FirebaseAuth auth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_preloader);
+
+        auth = FirebaseAuth.getInstance();
+
+        FirebaseUser currentUser = auth.getCurrentUser();
+
+        Intent intent;
+        if(currentUser != null) {
+            intent = new Intent(Preloader.this, MainActivity.class);
+        } else {
+            intent = new Intent(Preloader.this, Login.class);
+        }
+        startActivity(intent);
+        finish();
+    }
+}
