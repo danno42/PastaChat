7c7dbe23ad638fd28e0dfe3464676141491894d5
sergiozed7 <sergiozed16@gmail.com>
2020-11-03 20:15:01 +0100

    Login backend starting point

---
+++ app/src/main/java/com/example/pastachat/Login.java
@@ -0,0 +1,62 @@
+package com.example.pastachat;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class Login extends AppCompatActivity implements LoginInterface {
+
+    private FirebaseAuth auth;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        auth = FirebaseAuth.getInstance();
+    }
+
+    private void LoginEmailPassword(String email, String password) {
+        auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                Log.d("LOGIN", "Login operation completed");
+
+                if(task.isSuccessful()) {
+                    LoginSuccess();
+                } else {
+                    LoginFailed();
+                }
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.e("LOGIN", "Login error: " + e.getMessage());
+            }
+        });
+    }
+
+
+    @Override
+    public void LoginSuccess() {
+        Intent intent = new Intent(Login.this, MainActivity.class);
+
+        startActivity(intent);
+
+        finish();
+    }
+
+    @Override
+    public void LoginFailed() {
+
+    }
+}
